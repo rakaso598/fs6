a9fda428d3183f930b8a99a4eecaea3b2c858f55
rakaso598 <nexonaddress98@gmail.com>
2025-01-16 18:55:31 +0900

    wsl_init

---
+++ 3769/index.js
@@ -0,0 +1,24 @@
+const myNumberTag = document.getElementById('myNumber');
+const decreaseBtn = document.getElementById('decrease');
+const increaseBtn = document.getElementById('increase');
+
+let myNumber = +myNumberTag.textContent;
+
+decreaseBtn.onclick = function() {
+  myNumber--;
+  myNumber--;
+  myNumberTag.textContent = myNumber;
+};
+
+increaseBtn.onclick = function() {
+  myNumber++;
+  myNumberTag.textContent = myNumber;
+};
+
+const colorBtns = document.getElementsByClassName('color-btn');
+
+for (let btn of colorBtns) {
+  btn.onclick = function() {
+    myNumberTag.style.color = btn.dataset.color;
+  };
+}
