a8d86277edaa8fe232dc761a1adc829c478612dd
rakaso598 <nexonaddress98@gmail.com>
2025-02-06 11:34:46 +0900

    ADD btns code

--- 3769/index.js
+++ 3769/index.js
@@ -7,7 +7,7 @@ let myNumber = +myNumberTag.textContent;
 decreaseBtn.onclick = function() {
   myNumber--;
   myNumberTag.textContent = myNumber;
-  console.log(myNumberTag.textContent); // 콘솔에 화면과 같은 숫자 출력
+  console.log(myNumber); // 콘솔에 화면과 같은 숫자 출력
 };
 
 increaseBtn.onclick = function() {
@@ -25,3 +25,12 @@ for (let btn of colorBtns) {
     myNumberTag.style.color = btn.dataset.color;
   };
 }
+
+const btns = document.getElementById('btns');
+console.log(btns.children);
+console.log(btns.parentElement);
+console.log(btns.previousElementSibling);
+console.log(btns.nextElementSibling);
+
+console.log(colorBtns[1].previousElementSibling);
+console.log(colorBtns[1].nextElementSibling);
