84dbe9a7d90c34f5039d4fa502311f105aa2f8e1
rakaso598 <nexonaddress98@gmail.com>
2025-02-06 15:51:24 +0900

    ADD html manipulation code

--- 3769/index.js
+++ 3769/index.js
@@ -55,3 +55,102 @@ console.log(increaseXXX.parentElement.parentElement.children[1].children[3]);
 const greenBtn = increaseXXX.parentElement.parentElement.children[1].children[3];
 
 console.log("그린버튼= ", greenBtn);
+
+let list = document.getElementById('list');
+const addListBtn = document.getElementById('add-list');
+
+addListBtn.onclick = function () {
+  list.innerHTML += '<li>Hello</li>';
+  console.log(list.outerHTML);
+};
+
+const resetListBtn = document.getElementById('reset-list');
+resetListBtn.onclick = function () {
+  list.outerHTML = '<ul id="list"></ul>';
+  list = document.getElementById('list');
+};
+
+const updateListBtn = document.getElementById('update-list');
+updateListBtn.onclick = function () {
+
+  // <ul> 태그를 <ol> 태그로 변경
+  const itemsString = list.innerHTML; // 이너html 저장
+  console.log(list.innerHTML);
+  console.log(list.outerHTML);
+  list.outerHTML = '<ol id="list"></ol>'; // ol태그로 변경
+  list = document.getElementById('list'); // list 재선택
+  list.innerHTML = itemsString; // list의 이너html을 itemsString 다시 assign.
+
+}
+
+// innerHTML 은 줄의 안쪽(태그제외)
+// outerHTML 은 줄의 태그까지 포함 전부
+// // textContent 은 콘텐트(내용만)
+
+// changeListBtn.id // change-list
+// orange.className // color-btn orange
+
+const att1 = document.getElementById('att1');
+const att2 = document.getElementById('att2');
+
+att1.href = "https://google.com";
+att2.href = "https://google.com";
+
+console.log(att1.href);
+console.log(att2.href);
+
+// tag.getAttribute("href") // 속성을 출력해서 보여준다.
+// tag.setAttribute("href", "https://google.com") // 속성에 값을 설정한다.
+// tag.removeAttribute("href") // 속성을 제거한다.
+
+// att2.getAttribute();
+
+// att1.className = "color-btn orange";
+
+// att1.classList.add("done"); // 요소의 클래스트리스트에 클래스 추가
+// att1.classList.remove("done");
+// att1.classList.add("done");
+
+// att1.classList.contains("done"); // 요소의 클래스리스트에 done이 있는지 없는지
+
+// document.getElementById('style').textContent
+
+
+const item = document.getElementById('style')
+item.onclick = function () {
+  const isLarge = item.classList.contains('large');
+  const isBlue = item.classList.contains('blue-text');
+  
+  if (!isLarge && !isBlue) {
+    // 아무 효과가 없는 상태
+    item.classList.add("large");
+  } else if (isLarge && !isBlue) {
+    // 글자 크기가 커진 상태
+    item.classList.add('blue-text');
+  } else {
+    // 글자 크기가 크고 and 글자가 파란색인 상태
+    item.classList.remove('large');
+    item.classList.remove('blue-text');
+  }
+}
+
+
+// if (document.getElementById('style').classList.contains(null)) {
+//   document.getElementById('style').classList.add('step_1');
+// }
+
+// if (document.getElementById('style').classList.contains('step_1')) {
+//   document.getElementById('style').classList.add('step_2');
+// }
+
+// if (document.getElementById('style').classList.contains('step_2')) {
+//   document.getElementById('style').classList.remove('step_1','step_2');
+// }
+
+
+
+// // 폰트사이즈 포함
+// document.getElementById('style').classList.contains('font-size')
+
+// // 컬러 포함
+// document.getElementById('style').classList.contains('color')
