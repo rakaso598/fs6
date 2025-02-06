d59d76820424458d00cf84d12e1931050a3c756d
rakaso598 <nexonaddress98@gmail.com>
2025-02-06 10:48:11 +0900

    ADD 3769

--- 3769/index.js
+++ 3769/index.js
@@ -6,16 +6,19 @@ let myNumber = +myNumberTag.textContent;
 
 decreaseBtn.onclick = function() {
   myNumber--;
-  myNumber--;
   myNumberTag.textContent = myNumber;
+  console.log(myNumberTag.textContent); // 콘솔에 화면과 같은 숫자 출력
 };
 
 increaseBtn.onclick = function() {
+  console.log("increase btn cliecked!")
   myNumber++;
   myNumberTag.textContent = myNumber;
 };
 
 const colorBtns = document.getElementsByClassName('color-btn');
+console.log(colorBtns);
+console.log(colorBtns[0]);
 
 for (let btn of colorBtns) {
   btn.onclick = function() {
