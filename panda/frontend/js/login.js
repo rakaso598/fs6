c3927ddbb841161af53df16002901f6c587f009e
rakaso598 <nexonaddress98@gmail.com>
2025-02-11 10:10:21 +0900

    ADD email require msg

--- panda/frontend/js/login.js
+++ panda/frontend/js/login.js
@@ -1,4 +1,5 @@
 const inputEmail = document.getElementById('input-email');
+const emailError =  document.getElementById('email-error')
 
 inputEmail.addEventListener('focusin', (event) => {
   // 포커스인 테두리 설정
@@ -9,10 +10,14 @@ inputEmail.addEventListener('focusout', (event) => {
   event.preventDefault();
   // 포커스아웃 감지 시
   if (inputEmail.value === '') {
+    console.log('값이 없음');
     inputEmail.style.borderColor = 'red';
     inputEmail.style.borderWidth = '2px';
-    console.log('값이 없음');
+    emailError.textContent = '이메일을 입력해주세요.';
+    
   } else {
     inputEmail.style.borderColor = '#f3f4f6';
+    emailError.textContent = '';
+    emailError.style.color = 'red';
   }
 });
