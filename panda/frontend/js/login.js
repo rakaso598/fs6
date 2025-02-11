683e25b180dab73ffd89bed0ee8d59219a07b387
rakaso598 <nexonaddress98@gmail.com>
2025-02-11 09:33:41 +0900

    ADD focusout red

--- panda/frontend/js/login.js
+++ panda/frontend/js/login.js
@@ -1,7 +1,18 @@
-const emailInput = document.getElementById('input-email');
+const inputEmail = document.getElementById('input-email');
 
-emailInput.addEventListener('invalid', (event) => {
+inputEmail.addEventListener('focusin', (event) => {
+  // 포커스인 테두리 설정
+  inputEmail.style.borderColor = '#f3f4f6';
+})
+
+inputEmail.addEventListener('focusout', (event) => {
   event.preventDefault();
-  console.log('이벤트감지')
-  emailInput.style.borderColor = 'red';
+  // 포커스아웃 감지 시
+  if (inputEmail.value === '') {
+    inputEmail.style.borderColor = 'red';
+    inputEmail.style.borderWidth = '2px';
+    console.log('값이 없음');
+  } else {
+    inputEmail.style.borderColor = '#f3f4f6';
+  }
 });
