7a94c4dba1afb703f6ddbc0c478be7ba2d84fd97
rakaso598 <nexonaddress98@gmail.com>
2025-02-11 13:45:43 +0900

    FEAT: login validation

--- panda/frontend/js/login.js
+++ panda/frontend/js/login.js
@@ -1,13 +1,15 @@
 const inputEmail = document.getElementById('input-email');
 const emailError = document.getElementById('email-error');
-const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // 이메일 확인정규표현식
-let isInput = false;
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; // 이메일 정규표현식
+let isEmailOK = false;
 
 const inputPw = document.getElementById('input-pw');
 const pwError = document.getElementById('pw-error');
+let isPwOK = false;
 
 const btnSubmit = document.getElementById('btn-submit');
 
+updateButtonState();
 
 /* ************ Email 이메일 ************* */
 inputEmail.addEventListener('focusin', (event) => {
@@ -18,28 +20,27 @@ inputEmail.addEventListener('focusin', (event) => {
 inputEmail.addEventListener('focusout', (event) => {
   event.preventDefault();
   if (inputEmail.value === '') {
-    // 값이 없을 때
-    console.log('값이 없음');
+    // 이메일이 없을 때
+    console.log('이메일 입력 없음.');
     emailError.textContent = '이메일을 입력해주세요.';
     emailError.style.color = 'red';
     inputEmail.style.borderColor = 'red';
     inputEmail.style.borderWidth = '2px';
-    isInput = false;
+    isEmailOK = false;
   } else if (emailRegex.test(inputEmail.value) === false) {
     // 이메일 형식이 아닐때
-    console.log('이메일 형식이 아님');
+    console.log('이메일 형식이 아님.');
     emailError.textContent = '잘못된 이메일 형식입니다.';
     emailError.style.color = 'red';
     inputEmail.style.borderColor = 'red';
     inputEmail.style.borderWidth = '2px';
-
-    isInput = false;
+    isEmailOK = false;
   } else {
     emailError.textContent = '';
     inputEmail.style.borderColor = '#f3f4f6';
-    isInput = true;
+    isEmailOK = true;
   }
-
+  updateButtonState()
 });
 /* ************************************* */
 
@@ -53,27 +54,45 @@ inputPw.addEventListener('focusin', (event) => {
 inputPw.addEventListener('focusout', (event) => {
   event.preventDefault();
   if (inputPw.value === '') {
-    // 값이 없을 때
-    console.log('비밀번호가 없음');
+    // 비밀번호가 없을 때
+    console.log('비밀번호 입력 없음.');
     pwError.textContent = '비밀번호를 입력해주세요.';
     pwError.style.color = 'red';
     inputPw.style.borderColor = 'red';
     inputPw.style.borderWidth = '2px';
-    isInput = false;
+    isPwOK = false;
   } else if ((inputPw.value.length < 8) === true) {
     // 값이 8자 미만일 경우
-    console.log('비밀번호 8자 미만임.');
+    console.log('비밀번호가 8자 미만임.');
     pwError.textContent = '비밀번호를 8자 이상 입력해주세요.';
     pwError.style.color = 'red';
     inputPw.style.borderColor = 'red';
     inputPw.style.borderWidth = '2px';
-    isInput = false;
+    isPwOK = false;
   } else {
     pwError.textContent = '';
     inputPw.style.borderColor = '#f3f4f6';
-    isInput = true;
+    isPwOK = true;
   }
-
+  updateButtonState()
 });
+/* ************************************* */
+
+
+/* ************* 버튼 함수 ************* */
+function updateButtonState() {
+  btnSubmit.disabled = true;
+  if ((isEmailOK && isPwOK) === false) {
+    btnSubmit.disabled = true;
+    console.log('<로그인버튼> 입력값 누락!');
+    btnSubmit.style.backgroundColor = 'gray';
+    btnSubmit.style.cursor = 'default';
+  } else {
+    console.log('<로그인버튼> 조건을 만족했습니다.');
+    btnSubmit.style.backgroundColor = '#3691FF';
+    btnSubmit.style.cursor = 'pointer';
+    btnSubmit.disabled = false;
+  }
+}
 /* ************************************* */
 
