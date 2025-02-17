def9ee1d730fd4d633734392e72f2ccb6414b52f
rakaso598 <nexonaddress98@gmail.com>
2025-02-17 11:07:28 +0900

    250217-2-fetch

--- 250217/index.js
+++ 250217/index.js
@@ -1,15 +1,23 @@
 // fetch: 통신을 할때 서버에 요청을 보내는 함수, 서버의 응답이 함수의 결과값
 
-// https://learn.codeit.kr/api/employee
-
-const response = fetch('https://learn.codeit.kr/api/employee')
-response.then(res =>{
-  return res.json()
-})
-console.log(response) // Promise { <pending> }
-
 /*
 프로미스가 가진 메소드:
-- .then(함수)
-
+.then(함수)
 */
+
+
+// https://learn.codeit.kr/api/employees
+
+// const response = fetch('https://learn.codeit.kr/api/employees')
+// console.log(response) // Promise { <pending> }
+// const data = response.then(res => res.json())
+// console.log(data) // Promise { <pending> }
+// data.then(d=> {
+//   console.log(d)
+// })
+
+const response = fetch('https://learn.codeit.kr/api/employees')
+  .then(res => res.json())
+  .then(d => {
+    console.log(d)
+  })
