4f76b3bf8dc1d8e018acb84293514ea160a5acbd
rakaso598 <nexonaddress98@gmail.com>
2025-02-17 13:38:50 +0900

    250217-4-async

--- 250217/async.js
+++ 250217/async.js
@@ -3,26 +3,47 @@
 
 // 받은 문자열을 0.3초 후에 반환해주는 함수 만들자.
 
-// any타입 문자열을 인자로, 결과는 Promise<any>타입
+// any타입 문자열을 인자로, 결과는 Promise<any>타입인 함수
 const delayString = (string) => {
+
   // 프로미스는 함수를 인자로 받는데, 인자는 2가지가 있다.
   return new Promise((resolve, reject) => {
-    // fulfilled로 바꾼다. -> resolve
-    setTimeout(() => {
-      resolve(string); // 받은 인자를 resolve()
-    }, 300);
     
-    // rejected로 바꾼다. -> reject
+    // String이 문자열인지 어떻게 알 수 있을까
+    if (typeof string === "string") {
+      setTimeout(() => {
+        resolve(string);
+      }, 300); // fulfilled
+    } else {
+      reject("string only");
+    }
+
+    // reject 함수를 쓰는데, 이때 넘긴 데이터는 어디서 사용할 수 있을까?
+    // -> 프라미스 객체의 catch 메소드
   });
 };
 
-// const fetch = url => {
-//   return new Promise((resolve, reject)=>{
-//     if (응답성공) {
-//       resolve(Response 객체)
-//     }
-//   })
-// }
+// 프로미스 객체를 다루기 .then .catch
+delayString(123)
+  .then(result => {
+    console.log(result)
+  })
+  // reject일 시,
+  .catch(e => {
+    console.log(e)
+  })
+  .finally(()=>{
+    console.log("done")
+  })
 
-// fetch(...)
-// .then(res=>{})
+// await으로 처리하기 try~catch
+try {
+  const awaitDelayStringResult = await delayString(123)
+  console.log(awaitDelayStringResult)
+} catch (e) {
+  console.log(e)
+} finally {
+  console.log("done")
+}
+
+// console.log(await delayString("hello"));
