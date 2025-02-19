2d337384bceb7e0c3f6ffd60b4337459774a8849
rakaso598 <nexonaddress98@gmail.com>
2025-02-19 18:02:23 +0900

    feat: ArticleService & Request Method OK

--- panda/frontend/js/ArticleService.js
+++ panda/frontend/js/ArticleService.js
@@ -3,13 +3,13 @@ import axios from "axios";
 // 핸들러
 const requestErrorHandler = (e) => {
   if (e.response) {
-    return `요청 처리 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.`;
+    return `처리 중 오류가 발생했습니다. 없는 데이터일 수 있으므로 다른 값을 입력해보세요.`;
   } else {
-    return `현재 서버에 접속할 수 없습니다. 잠시 후 다시 시도해주세요.`;
+    return `서버에 접속하지 못했습니다. 잘못 만들어진 요청입니다.`;
   }
 };
 
-// 목록조회
+// 게시글목록조회
 const getArticleList = async (page, pageSize, orderBy = "recent", keyword) => {
   try {
     if (keyword) {
@@ -28,7 +28,7 @@ const getArticleList = async (page, pageSize, orderBy = "recent", keyword) => {
   }
 };
 
-// 개별조회
+// 게시글개별조회
 const getArticle = async (productId) => {
   try {
     const res = await axios.get(
@@ -40,44 +40,43 @@ const getArticle = async (productId) => {
   }
 };
 
-// 생성
-const createArticleURL = "https://panda-market-api-crud.vercel.app/articles";
-const createArticleData = {
-  image: "https://example.com/...",
-  content: "게시글 내용입니다.",
-  title: "게시글 제목입니다.",
-};
-const createArticle = async () => {
+// 게시글생성
+const createArticle = async (createArticleData) => {
   try {
-    const res = await axios.post(createArticleURL, createArticleData);
+    const res = await axios.post(
+      "https://panda-market-api-crud.vercel.app/articles",
+      createArticleData
+    );
     return res.data;
   } catch (e) {
     return requestErrorHandler(e);
   }
 };
 
-// 삭제
-const deleteArticleURL =
-  "https://panda-market-api-crud.vercel.app/articles/495";
-const deleteArticle = async () => {
+// 게시글삭제
+const deleteArticle = async (articleId, patchArticleData) => {
   try {
-    const res = await axios.delete(deleteArticleURL);
+    const res = await axios.delete(
+      `https://panda-market-api-crud.vercel.app/articles/${articleId}`
+    );
     return res.data;
   } catch (e) {
     return requestErrorHandler(e);
   }
 };
 
-// 수정
-const patchArticleURL = "https://panda-market-api-crud.vercel.app/articles/427";
-const patchArticleData = {
-  image: "https://example.com/...",
-  content: "게시글 내용입니다.",
-  title: "게시글 제목입니다.",
-};
-const patchArticle = async () => {
+// 게시글수정
+// const patchArticleData = {
+//   image: "https://example.com/...",
+//   content: "게시글 내용입니다.",
+//   title: "게시글 제목입니다.",
+// };
+const patchArticle = async (articleId, patchArticleData) => {
   try {
-    const res = await axios.patch(patchArticleURL, patchArticleData);
+    const res = await axios.patch(
+      `https://panda-market-api-crud.vercel.app/articles/${articleId}`,
+      patchArticleData
+    );
     return res.data;
   } catch (e) {
     return requestErrorHandler(e);
--- panda/frontend/js/main.js
+++ panda/frontend/js/main.js
@@ -19,30 +19,44 @@ import {
 // const articleList = await getArticleList(1, 10, undefined);
 // console.log(articleList);
 
-const articleList = await getArticleList(1, 10, undefined, "이름");
-console.log(articleList);
+// const articleList = await getArticleList(1, 10, undefined, "이름");
+// console.log(articleList);
 
 // 개별조회: articleId
 
-const article = await getArticle(3);
-console.log(article);
+// const article = await getArticle(3);
+// console.log(article);
 
-// const createArticleResponse = await createArticle();
+// 게시글생성:
+
+// const createArticleData = {
+//   image: "https://example.com/...",
+//   content: "게시글 내용입니다.",
+//   title: "게시글 제목입니다.",
+// };
+// const createArticleResponse = await createArticle(createArticleData);
 // console.log(createArticleResponse);
 
-// const deleteArticleResponse = await deleteArticle();
+// 게시글삭제
+
+// const deleteArticleResponse = await deleteArticle(447);
 // console.log(deleteArticleResponse);
 
-// const patchArticleResponse = await patchArticle();
+// 게시글수정
+
+// const patchArticleData = {
+//   image: "https://example.com/...",
+//   content: "게시글 내용입니다.",
+//   title: "게시글 제목입니다.",
+// };
+// const patchArticleResponse = await patchArticle(448, patchArticleData);
 // console.log(patchArticleResponse);
 
 // const productList = await getProductList();
 // console.log(productList);
 
 // const product = await getProduct();
-// console.log(product);
-
-// const createProductResponse = await createProduct();
+// console.log(product);// const createProductResponse = await createProduct();
 // console.log(createProductResponse);
 
 // const deleteProductResponse = await deleteProduct();
