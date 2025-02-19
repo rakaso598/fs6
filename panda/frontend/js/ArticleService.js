5026ec3acdf5d97a1e40c11bbc427f6749c535c5
rakaso598 <nexonaddress98@gmail.com>
2025-02-19 14:40:26 +0900

    Add: 2 Service files

--- panda/frontend/js/ArticleService.js
+++ panda/frontend/js/ArticleService.js
@@ -1,22 +1,44 @@
 import axios from "axios";
 
+// 핸들러
+const requestErrorHandler = (e) => {
+  if (e.response) {
+    console.error(`${e.response.status},${e.response.data}`);
+    return `${e.response.status},${e.response.data}`;
+  } else {
+    console.error(e.message);
+    return `${e.message}`;
+  }
+};
+
+// 아티클목록
 const getArticleListURL =
-  "https://panda-market-api-crud.vercel.app/articles?page=1&pageSize=10&orderBy=recent";
+  "https://panda-market-api-crud.vercel.app/articles?page=1&pageSize=10&orderBy=recentt";
 const getArticleList = async () => {
-  const res = await axios.get(getArticleListURL);
-  return res.data;
+  try {
+    const res = await axios.get(getArticleListURL);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const articleList = await getArticleList();
 console.log(articleList);
 
+// 아티클
 const getArticleURL = "https://panda-market-api-crud.vercel.app/articles/427";
 const getArticle = async () => {
-  const res = await axios.get(getArticleURL);
-  return res.data;
+  try {
+    const res = await axios.get(getArticleURL);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const article = await getArticle();
 console.log(article);
 
+// 아티클생성
 const createArticleURL = "https://panda-market-api-crud.vercel.app/articles";
 const createArticleData = {
   image: "https://example.com/...",
@@ -24,21 +46,31 @@ const createArticleData = {
   title: "게시글 제목입니다.",
 };
 const createArticle = async () => {
-  const res = await axios.post(createArticleURL, createArticleData);
-  return res.data;
+  try {
+    const res = await axios.post(createArticleURL, createArticleData);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const createArticleResponse = await createArticle();
 console.log(createArticleResponse);
 
+// 아티클삭제
 const deleteArticleURL =
   "https://panda-market-api-crud.vercel.app/articles/495";
 const deleteArticle = async () => {
-  const res = await axios.delete(deleteArticleURL);
-  return res.data;
+  try {
+    const res = await axios.delete(deleteArticleURL);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const deleteArticleResponse = await deleteArticle();
 console.log(deleteArticleResponse);
 
+// 아티클수정
 const patchArticleURL = "https://panda-market-api-crud.vercel.app/articles/427";
 const patchArticleData = {
   image: "https://example.com/...",
@@ -46,8 +78,12 @@ const patchArticleData = {
   title: "게시글 제목입니다.",
 };
 const patchArticle = async () => {
-  const res = await axios.patch(patchArticleURL, patchArticleData);
-  return res.data;
+  try {
+    const res = await axios.patch(patchArticleURL, patchArticleData);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const patchArticleResponse = await patchArticle();
 console.log(patchArticleResponse);
--- panda/frontend/js/ProductService.js
+++ panda/frontend/js/ProductService.js
@@ -1,22 +1,44 @@
 import axios from "axios";
 
+// 핸들러
+const requestErrorHandler = (e) => {
+  if (e.response) {
+    console.error(`${e.response.status},${e.response.data}`);
+    return `${e.response.status},${e.response.data}`;
+  } else {
+    console.error(e.message);
+    return `${e.message}`;
+  }
+};
+
+// 상품목록
 const getProductListURL =
   "https://panda-market-api-crud.vercel.app/products?page=1&pageSize=10&orderBy=recent";
 const getProductList = async () => {
-  const res = await axios.get(getProductListURL);
-  return res.data;
+  try {
+    const res = await axios.get(getProductListURL);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const productList = await getProductList();
 console.log(productList);
 
+// 상품
 const getProductURL = "https://panda-market-api-crud.vercel.app/products/496";
 const getProduct = async () => {
-  const res = await axios.get(getProductURL);
-  return res.data;
+  try {
+    const res = await axios.get(getProductURL);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const product = await getProduct();
 console.log(product);
 
+// 상품생성
 const createProductURL = "https://panda-market-api-crud.vercel.app/products";
 const createProductData = {
   images: ["https://example.com/..."],
@@ -26,21 +48,31 @@ const createProductData = {
   name: "상품 이름",
 };
 const createProduct = async () => {
-  const res = await axios.post(createProductURL, createProductData);
-  return res.data;
+  try {
+    const res = await axios.post(createProductURL, createProductData);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const createProductResponse = await createProduct();
 console.log(createProductResponse);
 
+// 상품삭제
 const deleteProductURL =
   "https://panda-market-api-crud.vercel.app/products/495";
 const deleteProduct = async () => {
-  const res = await axios.delete(deleteProductURL);
-  return res.data;
+  try {
+    const res = await axios.delete(deleteProductURL);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const deleteProductResponse = await deleteProduct();
 console.log(deleteProductResponse);
 
+// 상품수정
 const patchProductURL = "https://panda-market-api-crud.vercel.app/products/496";
 const patchProductData = {
   images: ["https://example.com/..."],
@@ -50,8 +82,12 @@ const patchProductData = {
   name: "상품 이름",
 };
 const patchProduct = async () => {
-  const res = await axios.patch(patchProductURL, patchProductData);
-  return res.data;
+  try {
+    const res = await axios.patch(patchProductURL, patchProductData);
+    return res.data;
+  } catch (e) {
+    return requestErrorHandler(e);
+  }
 };
 const patchProductResponse = await patchProduct();
 console.log(patchProductResponse);
